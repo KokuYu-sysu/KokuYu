cdc663e9f4b62a6e366300cf5d299a5f2e0da1c8
Jiahao Lin <linjh239@mail2.sysu.edu.cn>
2025-08-20 13:20:14 +0800

    Update create pairs.r

--- create pairs.r
+++ create pairs.r
@@ -1,13 +1,25 @@
-# When creating diseases pair (like comorbidity), it can be used to.
-create_pairs <- function(data, cols) {  # cols means the column number of variable you would combine
-  pairs <- combn(cols, 2)
-  for(i in 1:ncol(pairs)) {
-    col1 <- pairs[1,i]
-    col2 <- pairs[2,i]
-    new_col_name <- paste0(col1, "_X_", col2)
-    data[[new_col_name]] <- data[[col1]] + data[[col2]]  # '+' can sum up the value
+#' @param data A data frame containing the variables of interest.
+#' @param cols A character vector of column names to create pairs from.
+#' @param n The maximum number of variables to include in each pair.
+
+create_pairs <- function(data, cols, n = NULL) {
+  for (col in cols) {
+    if (!col %in% names(data)) {
+      stop(paste("Column", col, "not found in data."))
+    }
+  }
+
+  if (is.null(n)) {
+    n <- length(cols)
+  }
+
+  for (j in 2:n) {
+    pairs <- combn(cols, j)
+    for(i in 1:ncol(pairs)) {
+      combination_cols <- pairs[, i]
+      new_col_name <- paste(combination_cols, collapse = "_X_")
+      data[[new_col_name]] <- rowSums(data[combination_cols])
+    }
   }
   return(data)
 }
-# Output: 0 means both not, 1 means A or B, and 2 means A and B.
-# Theorically, it can add a parameter 'n' in order to create pair including any number of disease.
